a5ac55815bf077a6acf6f16f9db42fce5cb7b76e
JK <jerinisready@gmail.com>
2017-02-20 00:53:25 +0530

    Update credits.php

--- networklab/credits.php
+++ networklab/credits.php
@@ -12,7 +12,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com'; 		      // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'youremail@gmail.com';              // SMTP username
-$mail->Password = '(6@(k !t';                         // SMTP password   "CRACK IT"
+$mail->Password = 'yourpassword';                         // SMTP password   "CRACK IT"
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
